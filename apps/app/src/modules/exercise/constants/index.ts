b743aff14e025d1cc5a77bb9e9ea59fa27c26b55
Aleksandr Dergunov <dergunovs@mail.ru>
2024-10-21 19:58:04 +0300

    chore: github workflow

--- apps/app/src/modules/exercise/constants/index.ts
+++ apps/app/src/modules/exercise/constants/index.ts
@@ -2,5 +2,5 @@ export const URL_EXERCISE = '/exercises';
 export const URL_EXERCISE_CREATE = `${URL_EXERCISE}/create`;
 export const URL_EXERCISE_EDIT = `${URL_EXERCISE}/edit`;
 
-export const EXERCISE_WEIGHT_OPTIONS = [0, 9, 16];
+export const EXERCISE_WEIGHT_OPTIONS = [9, 16];
 export const EXERCISE_REPEATS_OPTIONS = [6, 8, 10, 12, 15, 20];
