a4af7d8e2c1b68d66a41dfc1504832cb03a3b51d
Aleksandr Dergunov <dergunovs@mail.ru>
2024-10-22 10:54:12 +0300

    fix: pwa language

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
         short_name: 'FiT',
         description: 'Фитнес трекер',
         theme_color: '#ffffff',
+        lang: 'ru',
         icons: [
           { src: 'pwa-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512.png', sizes: '512x512', type: 'image/png' },
